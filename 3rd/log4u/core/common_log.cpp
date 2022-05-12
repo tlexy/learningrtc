64704cf04e7b20c917332fa7a3b729ac0e33daaa
林兴 <7766640+linxing7@user.noreply.gitee.com>
2022-05-12 08:27:41 +0800

    添加日志库

---
+++ 3rd/log4u/core/common_log.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "common_log.h"
+
+namespace log4u
+{
+	void log(const char* text)
+	{
+		std::cout << text << " Line: " << __LINE__ << " File: " << __FILE__ << std::endl;
+	}
+
+	void loge(const char* text)
+	{
+		std::cout << "[ERROR]" << text << " Line: " << __LINE__ << " File: " << __FILE__ << std::endl;
+	}
+}
---
+++ 3rd/log4u/core/common_log.h
@@ -0,0 +1,10 @@
+﻿#ifndef LOG4U_COMMON_LOG_H
+#define LOG4U_COMMON_LOG_H
+
+namespace log4u
+{
+	void log(const char*);
+	void loge(const char*);
+}
+
+#endif // !LOG4U_COMMON_LOG_H
