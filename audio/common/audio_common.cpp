b81605c773bd47aef0b7649edd9c3727dc9797cf
林兴 <7766640+linxing7@user.noreply.gitee.com>
2022-05-12 08:44:37 +0800

    音频公共操作

---
+++ audio/common/audio_common.cpp
@@ -0,0 +1,43 @@
+﻿#include "audio_common.h"
+
+void AudioCommon::init_device()
+{
+	PaError err = Pa_Initialize();
+	if (err != paNoError)
+	{
+		return;
+	}
+	PaHostApiIndex hostCnt = Pa_GetHostApiCount();
+	for (int i = 0; i < hostCnt; ++i)
+	{
+		const PaHostApiInfo* hinfo = Pa_GetHostApiInfo(i);
+		if (hinfo->type == paWASAPI/* || hinfo->type == paDirectSound*/)
+		{
+			for (PaDeviceIndex hostDevice = 0; hostDevice < hinfo->deviceCount; ++hostDevice)
+			{
+				PaDeviceIndex deviceNum = Pa_HostApiDeviceIndexToDeviceIndex(i, hostDevice);
+				const PaDeviceInfo* dinfo = Pa_GetDeviceInfo(deviceNum);
+				if (dinfo != NULL && dinfo->maxInputChannels > 0)
+				{
+					_bgm_device_list[deviceNum] = dinfo;
+					_cb_bgm->addItem(QString::fromStdString(dinfo->name), QVariant(deviceNum));
+				}
+			}
+		}
+		else if (hinfo->type == paDirectSound)
+		{
+			for (PaDeviceIndex hostDevice = 0; hostDevice < hinfo->deviceCount; ++hostDevice)
+			{
+				PaDeviceIndex deviceNum = Pa_HostApiDeviceIndexToDeviceIndex(i, hostDevice);
+				const PaDeviceInfo* dinfo = Pa_GetDeviceInfo(deviceNum);
+				if (dinfo != NULL && dinfo->maxInputChannels > 0)
+				{
+					_mic_device_list[deviceNum] = dinfo;
+					_cb_mic->addItem(QString::fromStdString(dinfo->name), QVariant(deviceNum));
+					//将设备保存到录音对象中
+					PortRecorder::addPaDevice(deviceNum, dinfo);
+				}
+			}
+		}
+	}
+}
---
+++ audio/common/audio_common.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include <stdint.h>
+
+/// <summary>
+/// 音频设置初始化相关
+/// </summary>
+class AudioCommon
+{
+public:
+	static void init_device();
+};
+
