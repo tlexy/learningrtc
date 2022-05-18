7b6d95f2bde6ba3eda373e5726f85a236fd88ddf
林兴 <linxing@mail.wjhudong.com>
2022-05-18 18:04:05 +0800

    添加网络连接客户端

---
+++ pc_client/net/net_client.cpp
@@ -0,0 +1,45 @@
+﻿#include "net_client.h"
+#include <uvnet/core/tcp_server.h>
+#include <uvnet/core/tcp_connection.h>
+#include <uvnet/core/timer_event_loop.h>
+#include <uvnet/core/timer.h>
+#include <uvnet/core/event_loop.h>
+#include "common/util/sutil.h"
+
+NetTcpClient::NetTcpClient(std::shared_ptr<uvcore::EventLoop> loop, const uvcore::IpAddress& addr)
+	:TcpClient(loop, addr)
+{}
+
+void NetTcpClient::on_message(std::shared_ptr<uvcore::TcpConnection> ptr)
+{
+	std::string recv_msg((char*)ptr->get_inner_buffer()->read_ptr(), ptr->get_inner_buffer()->readable_size());
+	std::cout << "on message: " << recv_msg.c_str() << std::endl;
+
+	Json::Value json;
+	SUtil::parseJson("", json);
+
+	ptr->get_inner_buffer()->has_read(recv_msg.size());
+}
+
+void NetTcpClient::on_connection_close(std::shared_ptr<uvcore::TcpConnection> ptr)
+{}
+
+void NetTcpClient::connect()
+{
+	using namespace std::placeholders;
+	async_connect(std::bind(&NetTcpClient::on_connected, this, _1, _2));
+}
+
+void NetTcpClient::on_connected(int status, std::shared_ptr<uvcore::TcpConnection> ptr)
+{
+	if (status == 0)
+	{
+		std::string hello("hello, uvnet");
+		std::cout << "connected." << std::endl;
+		ptr->write(hello.c_str(), hello.size());
+	}
+	else
+	{
+		std::cout << "connect failed" << std::endl;
+	}
+}
---
+++ pc_client/net/net_client.h
@@ -0,0 +1,25 @@
+﻿#ifndef LEARNING_RTC_NET_CLIENT_H
+#define LEARNING_RTC_NET_CLIENT_H
+
+#include <utility>
+#include <uvnet/core/tcp_client.h>
+#include <iostream>
+#include <memory>
+#include <uvnet/core/ip_address.h>
+
+class NetTcpClient : public uvcore::TcpClient
+{
+public:
+	NetTcpClient(std::shared_ptr<uvcore::EventLoop> loop, const uvcore::IpAddress& addr);
+
+	virtual void on_message(std::shared_ptr<uvcore::TcpConnection> ptr);
+
+	virtual void on_connection_close(std::shared_ptr<uvcore::TcpConnection> ptr);
+
+	void connect();
+
+private:
+	void on_connected(int status, std::shared_ptr<uvcore::TcpConnection> ptr);
+};
+
+#endif
