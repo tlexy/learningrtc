934f60c5deb0d646ea019a8a066d96c0e8d92bf0
林兴 <7766640+linxing7@user.noreply.gitee.com>
2022-08-19 23:21:56 +0800

    添加随机发生器

---
+++ qos/emulator/net_random_emulator.cpp
@@ -0,0 +1,28 @@
+﻿#include "net_random_emulator.h"
+
+NetRandomEmulator::NetRandomEmulator()
+	:_rnd(1, 100)
+{}
+
+void NetRandomEmulator::set_loss_rate(int rate)
+{
+	if (rate < 0 || rate > 100)
+	{
+		return;
+	}
+	_loss_rate = rate;
+}
+
+void NetRandomEmulator::send_data(const char* data, int len)
+{
+	if (get_rate() == 0)
+	{
+		on_send_data(data, len);
+	}
+}
+
+int NetRandomEmulator::get_rate()
+{
+	int rnd = _rnd(_engine);
+	return rnd <= _loss_rate ? 1 : 0;
+}
---
+++ qos/emulator/net_random_emulator.h
@@ -0,0 +1,32 @@
+﻿#ifndef NET_RANDOM_EMULATOR_H
+#define NET_RANDOM_EMULATOR_H
+
+#include <stdint.h>
+#include <random>
+
+//网络随机丢包发生器
+class NetRandomEmulator
+{
+public:
+	NetRandomEmulator();
+	void set_loss_rate(int rate);
+
+	void send_data(const char* data, int len);
+
+	//没有发生丢包时，会调用这个函数进行真实的发送
+	virtual void on_send_data(const char* data, int len) = 0;
+
+private:
+	/// <summary>
+	/// 需要丢弃时，返回1
+	/// </summary>
+	/// <returns></returns>
+	int get_rate();
+
+private:
+	std::default_random_engine _engine;
+	std::uniform_int_distribution<int> _rnd;
+	int _loss_rate{0};
+};
+
+#endif
