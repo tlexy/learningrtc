396702043e7e77f4e736182ee6bf8fb8fdac0bc1
林兴 <7766640+linxing7@user.noreply.gitee.com>
2022-08-20 23:45:53 +0800

    新的rtp发送与接收器

---
+++ net_base/udp_channel.cpp
@@ -0,0 +1,76 @@
+﻿#include "udp_channel.h"
+#include <iostream>
+
+UdpChannel::UdpChannel(ChannelType ct)
+	:_type(ct)
+{}
+
+int UdpChannel::bind(std::shared_ptr<uvcore::UdpServer> server,
+	uvcore::Udp::UdpReceiveCallback2 cb, const uvcore::IpAddress& ip)
+{
+	using namespace std::placeholders;
+	if (!cb)
+	{
+		return 2;
+	}
+	_addr = ip;
+	_addr.set_valid(true);
+	_cb = cb;
+	if (_addr.getPort() == 0 && _type == ChannelType::Server)
+	{
+		std::cerr << "bind port is 0, but channel type is server." << std::endl;
+	}
+	_udp = server->addBind(_addr, std::bind(&UdpChannel::on_data_receive, this, _1, _2));
+	if (_udp)
+	{
+		return 0;
+	}
+	return 1;
+}
+
+void UdpChannel::on_data_receive(uvcore::Udp* udp, const struct sockaddr* addr)
+{
+	if (addr)
+	{
+		if (!_remote_addr())
+		{
+			uvcore::IpAddress raddr = uvcore::IpAddress::fromRawSocketAddress((sockaddr*)addr, sizeof(sockaddr_in));
+			_remote_addr = raddr;
+		}
+	}
+	if (!addr)
+	{
+		if (udp)
+		{
+			//udp总是有效的
+			delete udp;
+		}
+		_udp = nullptr;
+	}
+	//最终会回调用户传回来的参数
+	if (_cb)
+	{
+		//_udp与addr都有可能为nullptr，只要有一个为nullptr，就说明出错了
+		_cb(_udp, addr);
+	}
+}
+
+int UdpChannel::set_remote_addr(const uvcore::IpAddress& raddr)
+{
+	_remote_addr = raddr;
+}
+
+int UdpChannel::send(const char* data, int len)
+{
+	if (!_remote_addr())
+	{
+		//客户端发送必须有一个有效的远端地址
+		return -1;
+	}
+	if (!_udp)
+	{
+		return -2;
+	}
+	_udp->send2(data, len, _remote_addr);
+	return 0;
+}
---
+++ net_base/udp_channel.h
@@ -0,0 +1,64 @@
+﻿#ifndef UDP_CHANNEL_H
+#define UDP_CHANNEL_H
+
+#include <memory>
+#include <functional>
+#include <uvnet/core/ip_address.h>
+#include <uvnet/core/udp_server.h>
+#include <stdint.h>
+
+/// <summary>
+/// 发送与接收udp的类。
+/// 1. 特别注意在非io线程中进行bind，当bind失败的时候，bind也会返回一个有效的指针，
+/// 同时在io线程会调用on_data_receive
+/// 2. 在io线程中调用bind的时候，当bind失败的时候，bind也会返回一个空指针，
+/// 同时也会在io线程会调用on_data_receive
+/// 3.当new一个Udp对象失败时，首先会在调用线程中调用on_data_receive，然后返回nullptr
+/// </summary>
+class UdpChannel
+{
+public:
+	//using UdpDataCb = std::function<void(uvcore::Udp*, const struct sockaddr*)>;
+	enum class ChannelType
+	{
+		Client,
+		Server
+	};
+	/// <summary>
+	/// 目前来看，这个参数没什么用？？？
+	/// </summary>
+	/// <param name=""></param>
+	UdpChannel(ChannelType = ChannelType::Client);
+
+	/// <summary>
+	/// 在io线程中调用bind，如果返回0，则说明成功了，
+	/// 如果在非io线程中调用bind，即使返回0，可有可能是失败的
+	/// </summary>
+	/// <param name="server"></param>
+	/// <param name=""></param>
+	/// <param name="ip"></param>
+	/// <returns></returns>
+	int bind(std::shared_ptr<uvcore::UdpServer> server, 
+		uvcore::Udp::UdpReceiveCallback2, const uvcore::IpAddress& ip = uvcore::IpAddress());
+
+	/// <summary>
+	/// 作为一个客户端，发送前应该设置远端地址
+	/// </summary>
+	/// <param name=""></param>
+	/// <returns></returns>
+	int set_remote_addr(const uvcore::IpAddress&);
+	
+	int send(const char* data, int len);
+
+private:
+	void on_data_receive(uvcore::Udp*, const struct sockaddr*);
+
+private:
+	ChannelType _type;
+	uvcore::Udp* _udp{nullptr};
+	uvcore::IpAddress _addr;
+	uvcore::Udp::UdpReceiveCallback2 _cb;
+	uvcore::IpAddress _remote_addr;
+};
+
+#endif
